9d9e50637ead94b4fd7b2d942707eeb6ce4ededd
Achess <jealtzo26@gmail.com>
2022-02-27 20:28:42 -0600

    Adding activities

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.practica1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val buttonText = findViewById<Button>(R.id.button4)
+        buttonText.setOnClickListener {
+            val intent = Intent(this, TextActivity::class.java).apply {}
+            startActivity(intent)
+        }
+
+        val buttonCharts = findViewById<Button>(R.id.button3)
+        buttonCharts.setOnClickListener {
+            val intent = Intent(this, TextActivity::class.java).apply {}
+            startActivity(intent)
+        }
+
     }
 }
